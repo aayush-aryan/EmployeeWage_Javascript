5dad72cc206eac7737936856dfe0cbfd70ecbc62
aayush-aryan <log2aayusharyan@gmail.com>
2022-09-12 23:37:56 +0530

    added. checking employee is present or not

--- EmployeeWageManagement.js
+++ EmployeeWageManagement.js
@@ -1,1 +1,11 @@
 console.log("welcome to employee wage:");
+//UC1 : Checking employee is present or not 
+const IS_ABSENT=0;
+let empCheck=Math.floor(Math.random()*10) %3;
+{
+    if(empCheck==IS_ABSENT){
+        console.log("Employee is Absent");
+    }else{
+        console.log("Employee is precsent");
+    }
+}
